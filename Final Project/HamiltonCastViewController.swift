5b85e6f6bd08a111e7e08397b864aab32e45cd37
Louise <mckownlo@bearsmail.org>
2020-06-11 15:04:50 -0400

    WE MADE AN APP!!! HAHAHAHAHA BRANDON!

---
+++ Final Project/HamiltonCastViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HamiltonCastViewController.swift
+//  Final Project
+//
+//  Created by Louise Mckown on 6/11/20.
+//  Copyright © 2020 Tanvi Shanbhag. All rights reserved.
+//
+
+import UIKit
+
+class HamiltonCastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
